616662d5db9c0fcd7bfd700fc2113913f26cf758
Lukasz Gawel <l3moniak2@gmail.com>
2015-04-18 02:56:54 +0200

    add message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,8 +15,18 @@
 //= require_tree .
 //= require materialize-sprockets
 
+function showMessage(message) {
+    $('.message').text(message);
+    $('.message-container').fadeIn(500).delay(2000).fadeOut(500)
+}
+
 $(document).ready(function() {
     $("a[data-remote]").on('ajax:success', function(e, data, status, xhr) {
         $(this).siblings('.rating').html(data.rating);
+
+        if (status == "success")
+            showMessage('Voted!')
+        else
+            showMessage('Error!')
     });
 })
